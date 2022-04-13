d468fa9bc456651a3a1fb7cb8c3b09b1c94daad4
Alex Burton <azsb1g19@soton.ac.uk>
2022-04-13 23:40:00 +0100

    table joins

--- dataprov/prov_acquisition/other_pipeline/Joins.py
+++ dataprov/prov_acquisition/other_pipeline/Joins.py
@@ -1,40 +1,50 @@
 # Necessary packages
 from dataprov.prov_acquisition.prov_libraries import ProvenanceTracker
 import pandas as pd
+import os
+from dataprov.new_queries import create_mongo_pandas
 
 # n.b
 # If both key columns contain rows where the key is a null value, those rows will be matched against each other.
 # This is different from usual SQL join behaviour and can lead to unexpected results.
 
-df_a = pd.read_csv('Datasets\\table_a.csv')
-df_b = pd.read_csv('Datasets\\table_b.csv')
+if __name__ == '__main__':
+    prog_dir = os.path.dirname(os.path.realpath(__file__))
+    savepath = os.path.join(prog_dir, 'Results\\joins')
+    df_a = pd.read_csv(os.path.join(prog_dir,'Datasets\\table_a.csv'))
+    df_b = pd.read_csv(os.path.join(prog_dir,'Datasets\\table_b.csv'))
 
-#left join 
-tracker=ProvenanceTracker.ProvenanceTracker(df_a, '', 'left_join')
-tracker.set_description("Left Join on Column B")
-tracker.add_second_df(df_b)
-tracker.df = tracker.df.merge(df_b, on='B', how='left')
+    #left join 
+    tracker=ProvenanceTracker.ProvenanceTracker(df_a, '', os.path.join(savepath, 'left_join'))
+    tracker.set_description("Left Join on Column B")
+    tracker.add_second_df(df_b)
+    tracker.df = tracker.df.merge(df_b, on='B', how='left')
+    create_mongo_pandas.main('left_join_py_db', savepath)
 
-#right join
-tracker=ProvenanceTracker.ProvenanceTracker(df_a, '', 'right_join')
-tracker.set_description("Right Join on Column B")
-tracker.add_second_df(df_b)
-tracker.df = tracker.df.merge(df_b, on='B', how='right')
+    #right join
+    tracker=ProvenanceTracker.ProvenanceTracker(df_a, '', os.path.join(savepath, 'right_join'))
+    tracker.set_description("Right Join on Column B")
+    tracker.add_second_df(df_b)
+    tracker.df = tracker.df.merge(df_b, on='B', how='right')
+    create_mongo_pandas.main('right_join_py_db', savepath)
 
-#inner join
-tracker=ProvenanceTracker.ProvenanceTracker(df_a, '', 'inner_join')
-tracker.set_description("Inner Join on Column B")
-tracker.add_second_df(df_b)
-tracker.df = tracker.df.merge(df_b, on='B', how='inner')
+    #inner join
+    tracker=ProvenanceTracker.ProvenanceTracker(df_a, '', os.path.join(savepath, 'inner_join'))
+    tracker.set_description("Inner Join on Column B")
+    tracker.add_second_df(df_b)
+    tracker.df = tracker.df.merge(df_b, on='B', how='inner')
+    create_mongo_pandas.main('inner_join_py_db', savepath)
 
-#full outer join
-tracker=ProvenanceTracker.ProvenanceTracker(df_a, '', 'full_outer_join')
-tracker.set_description("Full Outer Join")
-tracker.add_second_df(df_b)
-tracker.df = tracker.df.merge(df_b, how='outer')
+    #full outer join
+    tracker=ProvenanceTracker.ProvenanceTracker(df_a, '', os.path.join(savepath, 'full_outer_join'))
+    tracker.set_description("Full Outer Join")
+    tracker.add_second_df(df_b)
+    tracker.df = tracker.df.merge(df_b, how='outer')
+    create_mongo_pandas.main('oute_join_py_db', savepath)
 
-#cross join
-tracker=ProvenanceTracker.ProvenanceTracker(df_a, '', 'cross_join')
-tracker.set_description("Cross Join")
-tracker.add_second_df(df_b)
-tracker.df = tracker.df.merge(df_b, how='cross')
+    #cross join
+    tracker=ProvenanceTracker.ProvenanceTracker(df_a, '', os.path.join(savepath, 'cross_join'))
+    tracker.set_description("Cross Join")
+    tracker.add_second_df(df_b)
+    tracker.df = tracker.df.merge(df_b, how='cross')
+    create_mongo_pandas.main('cross_join_py_db', savepath)
